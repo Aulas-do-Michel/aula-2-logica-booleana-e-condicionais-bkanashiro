de08e40a236a02ec593ba42504218b1b55d67db4
bkanashiro <163673470+bkanashiro@users.noreply.github.com>
2024-03-31 19:39:36 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -3,17 +3,8 @@
 
 Receba um número inteiro de um usuário. Se ele for par, imprima "Par". Se não, imprima "Ímpar".
 
-Exemplo:
-
-Digite um número:
-10
-
-Par
---------
-Digite um número:
-1
-
-Ímpar
-
-Dica: Lembre do comando de resto da divisão inteira!
-"""
+numero = int(input('Digite um número inteiro: '))
+if numero % 2 == 0:
+    print('O número é par')
+else:
+    print('O número é impar')
