827076a63ebd93ae1c256801e65fe82f0f5ab54a
bkanashiro <163673470+bkanashiro@users.noreply.github.com>
2024-04-01 11:13:00 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,10 +1,10 @@
 """
 #### Exercício 1
 
-Receba um número inteiro de um usuário. Se ele for par, imprima "Par". Se não, imprima "Ímpar".
+Receba um número inteiro de um usuário. Se ele for par, imprima "Par". Se não, imprima "Ímpar"."""
 
 numero = int(input('Digite um número inteiro: '))
 if numero % 2 == 0:
-    print('Par')
+    print("Par")
 else:
-    print('Ímpar')
+    print("Ímpar")
