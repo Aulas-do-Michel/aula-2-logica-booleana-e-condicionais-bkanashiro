4847ec357cd55549c09397b0d5537f61d6950760
bkanashiro <163673470+bkanashiro@users.noreply.github.com>
2024-04-01 11:07:26 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -12,7 +12,6 @@ Responde:
 Considere que a variante está no gene BRCA1 se estiver no cromossomo 17 (chr17), e se a posição estiver no intevalo de 41196312 a 41277500.
 
 Obs: Tirei a localização daqui: https://grch37.ensembl.org/Homo_sapiens/Gene/Summary?g=ENSG00000012048;r=17:41196312-41277500.
-___________________________________________________________________________________________________________________________________
 
 
 cromossomo = input('Digite o cromossomo: ')
@@ -21,4 +20,3 @@ if cromossomo == 'chr17' and 41196312 <= posição <= 41277500:
 	print ("Sim")
 else:
 	print ("Não")
-
